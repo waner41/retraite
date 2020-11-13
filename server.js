d55296dbcedc872961d1d01377582f6adb5796d9
Melet Erwan <meleterwan1@gmail.com>
2020-11-13 17:35:47 +0100

    package

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
-//Install express server
-const express = require('express');
-const path = require('path');
-
+import express from "express";
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/retraite'));
+app.use(static(__dirname + '/dist'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/retraite/index.html'));
+app.all('*', (req, res) => {
+  res.status(200).sendFile(__dirname + '/dist/retraite/index.html');
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
